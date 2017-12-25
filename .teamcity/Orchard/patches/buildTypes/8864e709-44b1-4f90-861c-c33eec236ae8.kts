860465bb352264d57272eaa3988074ad7219a51f
teamcity <teamcity@TeamCity>
2017-12-25 06:29:13 -0800

    TeamCity change in 'Orchard' project: parameters of 'Supersite_Publish' build configuration were updated

--- .teamcity/Orchard/patches/buildTypes/8864e709-44b1-4f90-861c-c33eec236ae8.kts
+++ .teamcity/Orchard/patches/buildTypes/8864e709-44b1-4f90-861c-c33eec236ae8.kts
@@ -15,10 +15,6 @@ create("c58659de-f5ce-44b2-ab74-0aaa2149b179", BuildType({
     id = "Orchard_Publish"
     name = "Supersite_Publish"
 
-    params {
-        param("Configuration", "Debug")
-    }
-
     vcs {
         root("Orchard_Develop")
 
